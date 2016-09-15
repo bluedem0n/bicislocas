14f36c3affdb723ae9556840be9893939cafe2df
Sthefany <Sthefany>
2016-09-15 11:45:38 -0500

    Validaciones

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 Lo que se realizó fue un  **validateForm**, de la siguiente forma:
 
-⋅⋅* Todos los campos son obligatorios, excepto los dos últimos. 
-..* Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
-..* Para los campos nombre y apellido la primera letra debe ser mayúscula
-..* Validar que el campo email tenga un formato válido. Ej: name@domain.com
-..* El campo password debe tener al menos 6 caracteres
-..* El campo password no puede ser igual a "password" ó "123456" ó "098754"
-..* El valor seleccionado de bicis, debe ser una de las opciones presentadas
-..* Si algún campo presenta error debe informarse al usuario por medio de un alert.
+  * Todos los campos son obligatorios, excepto los dos últimos. 
+  * Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
+  * Para los campos nombre y apellido la primera letra debe ser mayúscula
+  * Validar que el campo email tenga un formato válido. Ej: name@domain.com
+  * El campo password debe tener al menos 6 caracteres
+  * El campo password no puede ser igual a "password" ó "123456" ó "098754"
+  * El valor seleccionado de bicis, debe ser una de las opciones presentadas
+  * Si algún campo presenta error debe informarse al usuario por medio de un alert.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-/* Todos los campos son obligatorios, excepto los dos últimos. - Los campos nombre y apellido sólo deben permitir caracteres de la A-Z - Para los campos nombre y apellido la primera letra debe ser mayúscula - Validar que el campo email tenga un formato válido. Ej: name@domain.com - El campo password debe tener al menos 6 caracteres - El campo password no puede ser igual a "password" ó "123456" ó "098754" - El valor seleccionado de bicis, debe ser una de las opciones presentadas 4. Si algún campo presenta error debe informarse al usuario por medio de un alert. */
-
 function validateForm(){
-	/* Escribe tú código aquí */
+
+    //Todos los campos son obligatorios, excepto los dos últimos.
+
+    //Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
+
+
+    //Para los campos nombre y apellido la primera letra debe ser mayúscula
+
+    //El campo password debe tener al menos 6 caracteres
+    if(document.getElementById('input-password').value.length < 5){
+        alert("Inserte su contraseña de al menos 6 caracteres")
+    }
+    //El campo password no puede ser igual a "password" ó "123456" ó "098754"
+    var contraseña = document.getElementById('input-password').value;
+        if (contraseña == "123456" || contraseña == "password" || contraseña == "0987654"){
+            alert ("Ingresa contraseña valida");
+        }
+
+    //El valor seleccionado de bicis, debe ser una de las opciones presentadas
+     if(document.getElementsByTagName('select')[0].value == 0){
+            alert("Selecciona un tipo de bici");
+    }
+
 }
