5003936ecdcf992b7b98accd430a94dec486c149
Sthefany <Sthefany>
2016-09-15 10:40:06 -0500

    Agregando Readme

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Todos los campos son obligatorios, excepto los dos últimos. - Los campos nombre y apellido sólo deben permitir caracteres de la A-Z - Para los campos nombre y apellido la primera letra debe ser mayúscula - Validar que el campo email tenga un formato válido. Ej: name@domain.com - El campo password debe tener al menos 6 caracteres - El campo password no puede ser igual a "password" ó "123456" ó "098754" - El valor seleccionado de bicis, debe ser una de las opciones presentadas 4. Si algún campo presenta error debe informarse al usuario por medio de un alert. */
+
 function validateForm(){
 	/* Escribe tú código aquí */
 }
